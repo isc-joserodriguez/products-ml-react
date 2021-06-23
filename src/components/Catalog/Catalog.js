bb61a32ad1a96c7ce24be428dea9d2e155a9a59a
Jose Rodriguez <isc.joserodriguez@gmail.com>
2021-06-23 17:16:11 -0600

    [Pagination] Pagination

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Product from './Product/Product';
+import Pagination from '../Pagination/Pagination';
+
 import { Container, Row } from 'react-bootstrap';
+
 
 const Catalog = (props) => {
     const { products } = props;
+    const [page, setPage] = useState(1);
+    const [elementsByPage, setElementsByPage] = useState(30);
 
     return (
         <Container>
             <Row>
-                {products.map(product => (
+                {[...products].splice(elementsByPage * (page - 1), elementsByPage).map(product => (
                     <Product
                         key={product.id}
                         title={product.title}
@@ -21,6 +26,14 @@ const Catalog = (props) => {
                     />
                 ))}
             </Row>
+            <div className="d-flex justify-content-center">
+                <Pagination
+                    elements={products}
+                    page={page}
+                    setPage={setPage}
+                    elementsByPage={elementsByPage}
+                />
+            </div>
         </Container>
     )
 }
